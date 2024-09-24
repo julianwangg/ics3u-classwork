462a91ab8309099562ae4482b4cb3f52c8d8d8ba
Julian Wang <julianwang32@gmail.com>
2024-09-24 14:03:16 -0400

    Add files via upload

---
+++ your_schedule.py
@@ -0,0 +1,34 @@
+# Variables for courses and their teachers
+course1 = "English"
+course2 = "Computer Science"
+course3 = "Fitness"
+course4 = "Chemistry"
+course5 = "Advanced Functions"
+course6 = "Physics"
+course7 = "Religion"
+course8 = "Media Arts"
+
+teacher1 = "Ms Del Buono"
+teacher2 = "Mr Gallo"
+teacher3 = "Ms Coda"
+teacher4 = "Ms De Los Santos"
+teacher5 = "Math Teacher Unknown"
+teacher6 = "Physics Teacher Unknown"
+teacher7 = "Religion Teacher Unknown"
+teacher8 = "Art's Teacher Unknown"
+
+text = "MY SCHEDULE"
+
+#Printing the schedule
+print ("+------------------------------------------------------------+")
+print (f"|{text:60}|")
+print ("+------------------------------------------------------------+")
+print (f"| 1 | {course1:<20} | {teacher1:31} |")
+print (f"| 1 | {course2:<20} | {teacher2:31} |")
+print (f"| 1 | {course3:<20} | {teacher3:31} |")
+print (f"| 1 | {course4:<20} | {teacher4:31} |")
+print (f"| 1 | {course5:<20} | {teacher5:31} |")
+print (f"| 1 | {course6:<20} | {teacher6:31} |")
+print (f"| 1 | {course7:<20} | {teacher7:31} |")
+print (f"| 1 | {course8:<20} | {teacher8:31} |")
+print ("+------------------------------------------------------------+")
