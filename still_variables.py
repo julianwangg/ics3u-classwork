33d1af838d18f00d427a9de262895c6a90c53ecd
Julian Wang <julianwang32@gmail.com>
2024-09-24 13:35:36 -0400

    Add files via upload

---
+++ still_variables.py
@@ -0,0 +1,4 @@
+Name = "Juan Valdez"
+g = 2022
+
+print(f"my name is {Name} and I will be graduating on June 28th, {g}")
---
+++ using_variables.py
@@ -0,0 +1,7 @@
+Kobe_Bryant_Number = 24
+e = 2.123456789
+Later = "doing chemistry homework"
+
+print(f"In 2000, #{Kobe_Bryant_Number} of the Los Angeles Lakers was one of the best basketball players ever")
+print (f"while doing a math problem, I found the solution to be {e}")
+print (f"tonight, if you call me, I will be {Later}")
